10f90b46174dbbb2b2edb57c65dc82791e0c415f
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2024-12-20 11:03:07 -0800

    corrected code to point to the correct variable

--- autoupdate.sh
+++ autoupdate.sh
@@ -2,6 +2,6 @@
 
 LOG_FILE="/var/log/system_update.log"
 
-echo "System update started at $(date)" >> "LOG_FILE"
+echo "System update started at $(date)" >> "$LOG_FILE"
 apt update && apt upgrade -y >> "$LOG_FILE" 2>&1
-echo "System update completed at $(date)" >> "LOG_FILE"
+echo "System update completed at $(date)" >> "$LOG_FILE"
